82453c4822559df3462be2a9062247b10d00a1a6
emost22 <emoney96@naver.com>
2024-01-10 15:49:05 +0900

    Feat: add error code

--- src/main/java/com/spring/tming/global/meta/ResultCode.java
+++ src/main/java/com/spring/tming/global/meta/ResultCode.java
@@ -7,6 +7,7 @@ public enum ResultCode {
     SUCCESS(0, "정상 처리 되었습니다"),
 
     SYSTEM_ERROR(1000, "알 수 없는 에러가 발생했습니다."),
+    NOT_FOUND_FILE(1001, "파일을 찾을 수 없습니다."),
 
     NOT_FOUND_SAMPLE(2002, "샘플 데이터가 없습니다.");
 
