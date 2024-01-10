39a42ca4b3d4ebd8572d858d445bfb9cbdaa3d9a
emost22 <emoney96@naver.com>
2024-01-10 16:05:26 +0900

    Test: add delete file

--- src/test/java/com/spring/tming/global/s3/S3ProviderTest.java
+++ src/test/java/com/spring/tming/global/s3/S3ProviderTest.java
@@ -66,4 +66,19 @@ class S3ProviderTest {
         verify(amazonS3).putObject(any(), any(), any(), any());
         verify(amazonS3).getUrl(any(), any());
     }
+
+    @Test
+    @DisplayName("파일 삭제 테스트")
+    void 파일_삭제() {
+        // given
+        String filename = "test/images/sparta.png";
+        when(amazonS3.doesObjectExist(any(), any())).thenReturn(true);
+
+        // when
+        s3Provider.deleteImage(filename);
+
+        // then
+        verify(amazonS3).doesObjectExist(any(), any());
+        verify(amazonS3).deleteObject(any(), any());
+    }
 }
