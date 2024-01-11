2ca9d69cb25e11ffed47123387cfdb7b9fb63d63
RyanDJang <jangd6995@gmail.com>
2024-01-11 20:17:09 +0900

    [Feat] ResultCode에 이메일인증 관련 커스텀예외처리를 위한 에러메시지 추가

--- src/main/java/com/spring/tming/global/meta/ResultCode.java
+++ src/main/java/com/spring/tming/global/meta/ResultCode.java
@@ -9,7 +9,12 @@ public enum ResultCode {
     SYSTEM_ERROR(1000, "알 수 없는 에러가 발생했습니다."),
     NOT_FOUND_FILE(1001, "파일을 찾을 수 없습니다."),
 
-    NOT_FOUND_SAMPLE(2002, "샘플 데이터가 없습니다.");
+    NOT_FOUND_SAMPLE(2002, "샘플 데이터가 없습니다."),
+
+    // 이메일 유효성 검사
+    INVALID_EMAIL(1002, "올바른 이메일 주소가 아닙니다."),
+    EMPTY_EMAIL(1003, "이메일을 입력해주세요"),
+    EMAIL_SEND_ERROR(2000, "이메일 전송 중 오류가 발생했습니다.");
 
     private Integer code;
     private String message;
