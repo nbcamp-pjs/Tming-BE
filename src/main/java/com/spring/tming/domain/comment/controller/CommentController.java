460ad45e7e8a444d5bd979c393290e2621f77e8b
emost22 <emoney96@naver.com>
2024-01-13 20:52:36 +0900

    Feat: add delete comment

--- src/main/java/com/spring/tming/domain/comment/controller/CommentController.java
+++ src/main/java/com/spring/tming/domain/comment/controller/CommentController.java
@@ -1,7 +1,9 @@
 package com.spring.tming.domain.comment.controller;
 
+import com.spring.tming.domain.comment.dto.request.CommentDeleteReq;
 import com.spring.tming.domain.comment.dto.request.CommentSaveReq;
 import com.spring.tming.domain.comment.dto.request.CommentUpdateReq;
+import com.spring.tming.domain.comment.dto.response.CommentDeleteRes;
 import com.spring.tming.domain.comment.dto.response.CommentSaveRes;
 import com.spring.tming.domain.comment.dto.response.CommentUpdateRes;
 import com.spring.tming.domain.comment.service.CommentService;
@@ -9,6 +11,7 @@ import com.spring.tming.global.response.RestResponse;
 import com.spring.tming.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,5 +38,13 @@ public class CommentController {
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
         commentUpdateReq.setUserId(userDetails.getUser().getUserId());
         return RestResponse.success(commentService.updateComment(commentUpdateReq));
+    }
+
+    @DeleteMapping
+    public RestResponse<CommentDeleteRes> deleteComment(
+            @RequestBody CommentDeleteReq commentDeleteReq,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        commentDeleteReq.setUserId(userDetails.getUser().getUserId());
+        return RestResponse.success(commentService.deleteComment(commentDeleteReq));
     }
 }
