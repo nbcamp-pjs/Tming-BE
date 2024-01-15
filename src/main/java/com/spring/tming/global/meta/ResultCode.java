b1e9fb903d4fdbed33ae90791b70fd724fc2dff1
LeeSeungjun <78033706+lsj135779@users.noreply.github.com>
2024-01-15 16:43:17 +0900

    Fix: spotless apply in ResultCode.java

--- src/main/java/com/spring/tming/global/meta/ResultCode.java
+++ src/main/java/com/spring/tming/global/meta/ResultCode.java
@@ -33,7 +33,7 @@ public enum ResultCode {
     NOT_YET_LIKED_POST(4004, "아직 좋아요가 눌리지 않은 모집글입니다."),
     POST_INVALID_AUTHORIZATION(4005, "해당 모집글에 권한이 없습니다."),
     POST_INVALID_FILTER(4006, "지원하지 않는 전체조회 필터입니다."),
-    
+
     NOT_FOUND_COMMENT(5000, "댓글을 찾을 수 없거나 작성자가 아닙니다.");
 
     private Integer code;
