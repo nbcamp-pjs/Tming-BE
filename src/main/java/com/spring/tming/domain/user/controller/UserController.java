92a6e24fe59d7172f2a59f0a97079acc193d35f2
emost22 <emoney96@naver.com>
2024-01-15 15:18:34 +0900

    Feat: add getprofile api

--- src/main/java/com/spring/tming/domain/user/controller/UserController.java
+++ src/main/java/com/spring/tming/domain/user/controller/UserController.java
@@ -2,9 +2,12 @@ package com.spring.tming.domain.user.controller;
 
 import com.spring.tming.domain.user.dto.request.SignupReq;
 import com.spring.tming.domain.user.dto.response.SignupRes;
+import com.spring.tming.domain.user.dto.response.UserGetRes;
 import com.spring.tming.domain.user.service.UserService;
 import com.spring.tming.global.response.RestResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,10 @@ public class UserController {
     @PostMapping("/signup")
     public RestResponse<SignupRes> signup(@RequestBody SignupReq signupReq) {
         return RestResponse.success(userService.signup(signupReq));
+    }
+
+    @GetMapping("/{userId}")
+    public RestResponse<UserGetRes> getUserProfile(@PathVariable Long userId) {
+        return RestResponse.success(userService.getUserProfile(userId));
     }
 }
--- src/main/java/com/spring/tming/domain/user/service/UserServiceMapper.java
+++ src/main/java/com/spring/tming/domain/user/service/UserServiceMapper.java
@@ -1,10 +1,17 @@
 package com.spring.tming.domain.user.service;
 
+import com.spring.tming.domain.user.dto.response.UserGetRes;
+import com.spring.tming.domain.user.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
-public class UserServiceMapper {
+public interface UserServiceMapper {
 
-    UserServiceMapper SIGNUP = Mappers.getMapper(UserServiceMapper.class);
+    UserServiceMapper INSTANCE = Mappers.getMapper(UserServiceMapper.class);
+
+    // TODO add follower, following cnt
+    @Mapping(source = "job.description", target = "job")
+    UserGetRes toUserGetRes(User user);
 }
