74a1828eb92b66a22caa2ff3141d9deb8c57d1c6
이승준 <lsj135779@gmail.com>
2024-01-15 15:59:35 +0900

    Refactor: no return value in validate

--- src/main/java/com/spring/tming/global/validator/PostValidator.java
+++ src/main/java/com/spring/tming/global/validator/PostValidator.java
@@ -5,11 +5,11 @@ import static com.spring.tming.global.meta.ResultCode.NOT_FOUND_POST;
 import static com.spring.tming.global.meta.ResultCode.POST_INVALID_AUTHORIZATION;
 import static com.spring.tming.global.meta.ResultCode.POST_INVALID_FILTER;
 
-import com.spring.tming.domain.post.entity.Job;
+import com.spring.tming.global.meta.Job;
 import com.spring.tming.domain.post.entity.Post;
 import com.spring.tming.domain.post.entity.PostLike;
-import com.spring.tming.domain.post.entity.Skill;
-import com.spring.tming.domain.post.enums.Type;
+import com.spring.tming.global.meta.Skill;
+import com.spring.tming.global.meta.Type;
 import com.spring.tming.domain.user.entity.User;
 import com.spring.tming.global.exception.GlobalException;
 import java.util.Objects;
@@ -46,33 +46,27 @@ public class PostValidator {
         return !Objects.equals(post.getUser().getUserId(), user.getUserId());
     }
 
-    public static Type checkIsValidType(Type type) {
-        Type checkedType;
+    public static void checkIsValidType(Type type) {
         try {
-            checkedType = Type.valueOf(String.valueOf(type));
+            Type checkedType = Type.valueOf(String.valueOf(type));
         } catch (IllegalArgumentException e) {
             throw new GlobalException(POST_INVALID_FILTER);
         }
-        return checkedType;
     }
 
-    public static Skill checkIsValidSkill(Skill skill) {
-        Skill checkedSkill;
+    public static void checkIsValidSkill(Skill skill) {
         try {
-            checkedSkill = Skill.valueOf(String.valueOf(skill));
+            Skill checkedSkill = Skill.valueOf(String.valueOf(skill));
         } catch (IllegalArgumentException e) {
             throw new GlobalException(POST_INVALID_FILTER);
         }
-        return checkedSkill;
     }
 
-    public static Job checkIsValidJob(Job job) {
-        Job checkedJob;
+    public static void checkIsValidJob(Job job) {
         try {
-            checkedJob = Job.valueOf(String.valueOf(job));
+            Job checkedJob = Job.valueOf(String.valueOf(job));
         } catch (IllegalArgumentException e) {
             throw new GlobalException(POST_INVALID_FILTER);
         }
-        return checkedJob;
     }
 }
