3ce11527ad7ca0bc0272775f4c14c50c218b5bed
emost22 <emoney96@naver.com>
2024-01-16 13:28:41 +0900

    Feat: add follower following cnt

--- src/main/java/com/spring/tming/domain/user/service/UserServiceMapper.java
+++ src/main/java/com/spring/tming/domain/user/service/UserServiceMapper.java
@@ -4,18 +4,30 @@ import com.spring.tming.domain.user.dto.response.FollowerGetRes;
 import com.spring.tming.domain.user.dto.response.FollowingGetRes;
 import com.spring.tming.domain.user.dto.response.LoginRes;
 import com.spring.tming.domain.user.dto.response.UserGetRes;
+import com.spring.tming.domain.user.entity.Follow;
 import com.spring.tming.domain.user.entity.User;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.util.CollectionUtils;
 
 @Mapper
 public interface UserServiceMapper {
 
     UserServiceMapper INSTANCE = Mappers.getMapper(UserServiceMapper.class);
 
-    // TODO add follower, following cnt
+    @Mapping(source = "followings", target = "following")
+    @Mapping(source = "followers", target = "follower")
+    default int toFollowCnt(List<Follow> follows) {
+        if (CollectionUtils.isEmpty(follows)) {
+            return 0;
+        }
+        return follows.size();
+    }
+
+    @Mapping(source = "followers", target = "follower")
+    @Mapping(source = "followings", target = "following")
     @Mapping(source = "job.description", target = "job")
     UserGetRes toUserGetRes(User user);
 
