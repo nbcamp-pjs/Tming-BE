01b8ea26e8cd1075eb73c710029297db859aa876
RyanDJang <jangd6995@gmail.com>
2024-01-17 19:26:19 +0900

    [FEAT] 모집글 신청을 위한 Entity 작성

---
+++ src/main/java/com/spring/tming/domain/postApply/PostApply.java
@@ -0,0 +1,40 @@
+package com.spring.tming.domain.postApply;
+
+import com.spring.tming.domain.post.entity.Job;
+import com.spring.tming.domain.post.entity.Post;
+import com.spring.tming.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.mapstruct.control.MappingControl;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_post_apply")
+public class PostApply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long applicationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Job job;
+
+    @Builder
+    private PostApply(Post post, User user, Job job){
+        this.post = post;
+        this.user = user;
+        this.job = job;
+    }
+}
