75293804dbb106e7ae4f3ea87e94f129884ac557
jeendale <jeekoo7203@gmail.com>
2024-01-19 10:29:33 +0900

    fix : spotless실행

--- src/main/java/com/spring/tming/domain/chat/controller/RoomController.java
+++ src/main/java/com/spring/tming/domain/chat/controller/RoomController.java
@@ -2,18 +2,12 @@ package com.spring.tming.domain.chat.controller;
 
 import com.spring.tming.domain.chat.dto.request.RoomSaveReq;
 import com.spring.tming.domain.chat.dto.response.RoomSaveRes;
-import com.spring.tming.domain.chat.repository.ChatMemberRepository;
-import com.spring.tming.domain.chat.repository.ChatRoomRepository;
 import com.spring.tming.domain.chat.service.RoomService;
-import com.spring.tming.domain.user.repository.UserRepository;
 import com.spring.tming.global.response.RestResponse;
 import com.spring.tming.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
