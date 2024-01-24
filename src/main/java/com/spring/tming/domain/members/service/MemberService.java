eb31d520c498c35f76aee6d84023cc55b0c68c28
RyanDJang <jangd6995@gmail.com>
2024-01-24 11:07:00 +0900

    [FEAT][REFACTOR] (1) 팀원신청 승인에 관한 dto, 컨트롤러, 서비스, 매퍼 작성 (2) 팀원신청 승인에 관한 MemberValidator 추가 작성 (3) MemberValidator 에 해당하는 ResultCode 추가 작성

--- src/main/java/com/spring/tming/domain/members/service/MemberService.java
+++ src/main/java/com/spring/tming/domain/members/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.spring.tming.domain.members.service;
 
 import com.spring.tming.domain.members.dto.request.EmitMemberReq;
+import com.spring.tming.domain.members.dto.request.MemberAdmitReq;
 import com.spring.tming.domain.members.dto.response.EmitMemberRes;
+import com.spring.tming.domain.members.dto.response.MemberAdmitRes;
+import com.spring.tming.domain.members.dto.response.MemberGetRes;
+import com.spring.tming.domain.members.dto.response.MemberGetResList;
 import com.spring.tming.domain.members.entity.Member;
 import com.spring.tming.domain.members.repository.MemberRepository;
 import com.spring.tming.domain.post.entity.Post;
@@ -11,9 +15,10 @@ import com.spring.tming.domain.user.repository.UserRepository;
 import com.spring.tming.global.validator.MemberValidator;
 import com.spring.tming.global.validator.PostValidator;
 import com.spring.tming.global.validator.UserValidator;
-import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +29,34 @@ public class MemberService {
     private final PostRepository postRepository;
 
     @Transactional
+    public MemberAdmitRes admitMember(MemberAdmitReq memberAdmitReq) {
+        User user = getUser(memberAdmitReq.getUserId());
+        Post post = getPost(memberAdmitReq.getPostId(), user);
+        Member member = memberRepository.findByPostAndUser(post, user);
+        MemberValidator.checkAlreadyAdmitted(member);
+        MemberValidator.checkCancelBeforeAdmit(member);
+        memberRepository.save(
+                Member.builder().user(user).post(post).job(memberAdmitReq.getJob()).build());
+        return new MemberAdmitRes();
+    }
+
+    @Transactional
     public EmitMemberRes emitMember(User user, EmitMemberReq request) {
         Post post = getPost(request.getPostId(), user);
         User emitUser = getUser(request.getUserId());
         Member member = getMember(post, emitUser);
         memberRepository.delete(member);
         return new EmitMemberRes();
+    }
+
+    @Transactional(readOnly = true)
+    public MemberGetResList getMembers(Long postId) {
+        List<MemberGetRes> memberGetResList =
+                MemberServiceMapper.INSTANCE.toMemberGetResList(memberRepository.findByPostPostId(postId));
+        return MemberGetResList.builder()
+                .members(memberGetResList)
+                .total(memberGetResList.size())
+                .build();
     }
 
     private Post getPost(Long postId, User user) {
--- src/main/java/com/spring/tming/global/validator/MemberValidator.java
+++ src/main/java/com/spring/tming/global/validator/MemberValidator.java
@@ -4,6 +4,7 @@ import static com.spring.tming.global.meta.ResultCode.NOT_FOUND_MEMBER;
 
 import com.spring.tming.domain.members.entity.Member;
 import com.spring.tming.global.exception.GlobalException;
+import com.spring.tming.global.meta.ResultCode;
 
 public class MemberValidator {
 
@@ -11,6 +12,22 @@ public class MemberValidator {
         if (isNullMember(member)) {
             throw new GlobalException(NOT_FOUND_MEMBER);
         }
+    }
+
+    public static void checkAlreadyAdmitted(Member member) {
+        if (isExistMember(member)) {
+            throw new GlobalException(ResultCode.ALREADY_ADMITTED_MEMBER);
+        }
+    }
+
+    public static void checkCancelBeforeAdmit(Member member) {
+        if (!isNullMember(member)) {
+            throw new GlobalException(ResultCode.ALREADY_CANCELED_MEMBER);
+        }
+    }
+
+    private static boolean isExistMember(Member member) {
+        return member != null;
     }
 
     private static boolean isNullMember(Member member) {
