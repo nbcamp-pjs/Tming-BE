18d2677f536860229fddfb1d975248389475643f
dong <ioae7979@gmail.com>
2024-01-29 17:50:07 +0900

    refactor: jwt 토큰 예외처리

--- src/main/java/com/spring/tming/global/jwt/JwtUtil.java
+++ src/main/java/com/spring/tming/global/jwt/JwtUtil.java
@@ -1,5 +1,7 @@
 package com.spring.tming.global.jwt;
 
+import com.spring.tming.global.exception.GlobalException;
+import com.spring.tming.global.meta.ResultCode;
 import com.spring.tming.global.validator.TokenValidator;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
@@ -74,14 +76,17 @@ public class JwtUtil {
             return true;
         } catch (SecurityException | MalformedJwtException | SignatureException e) {
             log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+            throw new GlobalException(ResultCode.INVALID_TOKEN);
         } catch (ExpiredJwtException e) {
             log.error("Expired JWT token, 만료된 JWT token 입니다.");
+            throw new GlobalException(ResultCode.EXPIRED_TOKEN);
         } catch (UnsupportedJwtException e) {
             log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+            throw new GlobalException(ResultCode.UNSUPPORTED_TOKEN);
         } catch (IllegalArgumentException e) {
             log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+            throw new GlobalException(ResultCode.EMPTY_TOKEN);
         }
-        return false;
     }
 
     public Claims getUserInfoFromToken(String token) {
