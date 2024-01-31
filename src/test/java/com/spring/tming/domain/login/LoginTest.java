ce36bc255e430d90d9c75cf31a597e67ef44a2e7
dong <ioae7979@gmail.com>
2024-01-31 14:38:52 +0900

    style: 주석문 제거

--- src/test/java/com/spring/tming/domain/login/LoginTest.java
+++ src/test/java/com/spring/tming/domain/login/LoginTest.java
@@ -4,7 +4,6 @@ import static com.spring.tming.test.UserTest.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.spring.tming.domain.user.entity.User;
@@ -23,9 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-// @WebMvcTest(controllers = {SecurityConfig.class})
 @SpringBootTest
-// @Transactional
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 public class LoginTest {
@@ -76,9 +73,7 @@ public class LoginTest {
         String jsonReq = "{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}";
 
         mockMvc
-                .perform(post("/v1/users/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonReq))
+                .perform(post("/v1/users/login").contentType(MediaType.APPLICATION_JSON).content(jsonReq))
                 .andDo(print())
                 .andExpect(unauthenticated());
     }
